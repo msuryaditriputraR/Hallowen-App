697d23faa22bddf56c9d3b65158d9103fa853dca
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-10-19 19:19:04 +0800

    add navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,28 @@
 /*=== SHOW MENU ===*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
-/*=== REMOVE MENU MOBILE ===*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=== REMOVE MENU MOBILE WHEN CLICK MENU ===*/
+const navLink = document.querySelectorAll(".nav__link");
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+});
 
 /*=== HOME SWIPER ===*/
 
