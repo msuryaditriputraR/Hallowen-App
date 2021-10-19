6251ae180d0d50a55c985a83b9c1f148c4e6a05b
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-10-19 10:48:09 +0800

    project setup

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*=== SHOW MENU ===*/
+
+/*=== REMOVE MENU MOBILE ===*/
+
+/*=== HOME SWIPER ===*/
+
+/*=== CHANGE BACKGROUND HEADER ===*/
+
+/*=== NEW SWIPER ===*/
+
+/*=== SCROLL SECTIONS ACTIVE LINK ===*/
+
+/*=== SHOW SCROLL UP ===*/
+
+/*=== SCROLL REVEAL ANIMATION ===*/
