43350eae6e34073af51d671da31ed431d0d8d16e
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-10-20 09:45:59 +0800

    change active link when scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,28 @@ const newSwiper = new Swiper('.new-swiper', {
 });
 
 /*=== SCROLL SECTIONS ACTIVE LINK ===*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(section => {
+        const sectionHeight = section.offsetHeight,
+            sectionTop = section.offsetTop - 50,
+            sectionId = section.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.add('active-link');
+        } else {
+            document
+                .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
 
 /*=== SHOW SCROLL UP ===*/
 
